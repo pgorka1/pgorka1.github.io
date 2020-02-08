4e0a7052ddecdcb40ce167f52eb68ab5a984d142
pgorka1 <night.knight12358@gmail.com>
2020-02-08 20:13:36 +0100

    gitignore changed

---
+++ build/precache-manifest.fd48b3f193796bbf33c8ea81b0fbec8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee9ae415ea10cb90d40b7b4bdbb3ad3e",
+    "url": "pgorka1.github.io/index.html"
+  },
+  {
+    "revision": "ef5dd579fb493a8a84e0",
+    "url": "pgorka1.github.io/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2916709bdd73c31efc1c",
+    "url": "pgorka1.github.io/static/js/2.6a2a7794.chunk.js"
+  },
+  {
+    "revision": "ef5dd579fb493a8a84e0",
+    "url": "pgorka1.github.io/static/js/main.a3a0a0fb.chunk.js"
+  },
+  {
+    "revision": "2e3dbfaaecba4f0995a5",
+    "url": "pgorka1.github.io/static/js/runtime-main.24b359a1.js"
+  }
+]);
